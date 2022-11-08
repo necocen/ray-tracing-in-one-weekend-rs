5aa10fb51e038fd239638df016c76d0033966c96
necocen <necocen@gmail.com>
2022-11-08 23:20:08 +0900

    Section 5.2

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,18 @@ fn main() {
 }
 
 fn ray_color(ray: Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let t = 0.5 * (ray.direction.unit().y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> bool {
+    let oc = ray.origin - center;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * oc.dot(ray.direction);
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
 }
