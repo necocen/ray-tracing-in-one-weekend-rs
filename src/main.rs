3e1857c4b04ec212e17076d6890c348a368dadda
necocen <necocen@gmail.com>
2022-11-08 23:10:52 +0900

    Section 4.2

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,47 @@
-use vec3::Color;
+use ray::Ray;
+use vec3::{Color, Point3, Vec3};
 
 mod ray;
 mod vec3;
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = ((image_width as f64) / aspect_ratio) as i32;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    // Render
 
     println!("P3\n{image_width} {image_height}\n255");
 
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {j}");
         for i in 0..image_width {
-            let c = Color::new(
-                (i as f64) / ((image_width - 1) as f64),
-                (j as f64) / ((image_width - 1) as f64),
-                0.25f64,
+            let u = (i as f64) / ((image_width - 1) as f64);
+            let v = (j as f64) / ((image_height - 1) as f64);
+            let ray = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let c = ray_color(ray);
             _ = c.write(&mut std::io::stdout());
         }
     }
     eprint!("\nDone.\n");
+}
+
+fn ray_color(ray: Ray) -> Color {
+    let t = 0.5 * (ray.direction.unit().y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
