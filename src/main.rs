8f58f0835d6c3aac9bad082bbda50f3d12c4e5da
necocen <necocen@gmail.com>
2022-11-11 23:33:01 +0900

    Section 10.5

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,11 @@ fn main() {
             &material_left,
         )),
         Box::new(Sphere::new(
+            Point3::new(-1.0, 0.0, -1.0),
+            -0.4,
+            &material_left,
+        )),
+        Box::new(Sphere::new(
             Point3::new(1.0, 0.0, -1.0),
             0.5,
             &material_right,
