d8e762a21ef2246766320acdd53d775e714bc5c3
necocen <necocen@gmail.com>
2022-11-30 00:32:11 +0900

    NextWeek Section 7.3

--- src/hittables.rs
+++ src/hittables.rs
@@ -5,6 +5,7 @@ mod hittable;
 mod hittable_vec;
 mod moving_sphere;
 mod sphere;
+mod xy_rect;
 
 pub use aabb::Aabb;
 pub use bvh_tree::BvhTree;
@@ -13,3 +14,4 @@ pub use hittable::Hittable;
 pub use hittable_vec::HittableVec;
 pub use moving_sphere::MovingSphere;
 pub use sphere::Sphere;
+pub use xy_rect::XyRect;
